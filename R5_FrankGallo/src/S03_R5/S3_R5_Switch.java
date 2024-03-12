a3723b01fff4b7a54d251f5b6f6b9d3699c57b33
Frank Gallo <111780777+FrankGalloSanchez@users.noreply.github.com>
2024-03-11 21:34:33 -0500

    Switch

---
+++ R5_FrankGallo/src/S03_R5/S3_R5_Switch.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S03_R5;
+
+/**
+ *
+ * @author user
+ */
+public class S3_R5_Switch {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        switch (a) {
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+            default:
+                System.out.println("No se encontraron las Opciones");
+                break;
+        }
+
+    }
+}
