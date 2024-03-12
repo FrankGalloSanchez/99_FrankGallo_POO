90f6033b79e80c9384df9b5122617c5d40dc5ac4
Frank Gallo <111780777+FrankGalloSanchez@users.noreply.github.com>
2024-03-11 21:57:34 -0500

    DoWhile

---
+++ R5_FrankGallo/src/S03_R5/S3_R5_DoWhile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S03_R5;
+
+/**
+ *
+ * @author user
+ */
+public class S3_R5_DoWhile {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+
+        do {
+            System.out.println("Hola");
+        } while (a == 200);
+    }
+}
